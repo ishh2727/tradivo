fd4026bf107073cceb99d4917ad1cb6fbcf6e6fc
ishh2727 <ishbsk6@gmail.com>
2026-08-17 22:02:33 -0400

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // Supabase connection
-const SUPABASE_URL = "https://emtgjwrfxsxainwmsgkr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://emtgjwrfxsxainwmsgkr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_lEgj73XsD3_MG9DdDETZXg_Rjjjoy-2";
 
 const db = window.supabase.createClient(
